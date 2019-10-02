726710fcf86733fa12a21c64e1a95a6edbdf07dd
Ayse Erduran <44211446+Ayse-Erduran@users.noreply.github.com>
2019-10-02 16:32:24 -0400

    Update PS 2.2.py

--- ProblemSet_2/PS 2.2.py
+++ ProblemSet_2/PS 2.2.py
@@ -17,19 +17,19 @@ annualInterestRate: annual interest rate as a decimal
 Returns the lowest monthly payment that will pay off all debt in under 1 year. 
 """
 
-balance = 3329
-annualInterestRate = 0.2
-initBalance = balance 
 
-minFixed = 0
+monthlyPaymentRate = 0
+init_balance = balance
+monthlyInterestRate = annualInterestRate/12
 
-while True:
+while balance > 0:
     for i in range(12):
-        balance = (balance - minFixed) + (balance - minFixed) * annualInterestRate / 12
+        balance = balance - monthlyPaymentRate + ((balance - monthlyPaymentRate) * monthlyInterestRate)
     if balance > 0:
-        minFixed += 10
-        balance = initBalance
-    else:
-        False
-        
-print("Lowest payment: " + str(minFixed))
+        monthlyPaymentRate += 10
+        balance = init_balance
+    elif balance <= 0:
+        break
+print('Lowest Payment:', monthlyPaymentRate)
+
+
